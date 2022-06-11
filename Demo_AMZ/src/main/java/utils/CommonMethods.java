4329e1dcf09508bb75a4bcad92b766a138adbff2
haritha789 <65031841+haritha789@users.noreply.github.com>
2022-06-11 16:44:53 +0400

    added test steps

--- Demo_AMZ/src/main/java/utils/CommonMethods.java
+++ Demo_AMZ/src/main/java/utils/CommonMethods.java
@@ -6,16 +6,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 public class CommonMethods {
 
     public static WebElement clickElement(WebDriver driver,WebElement webElement) {
-        WebDriverWait wait = new WebDriverWait(driver, 100);
+        WebDriverWait wait = new WebDriverWait(driver, 60);
         WebElement element = wait.ignoring(StaleElementReferenceException.class).until(ExpectedConditions.elementToBeClickable(webElement));
         element.click();
         return element;
        }
 
-       public static String getElementText(WebElement webElement){
-        return webElement.getText();
-       }
+    public static void switchToSecondWindowTab(WebDriver driver) {
+        driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+        for(String winHandle : driver.getWindowHandles()){
+            driver.switchTo().window(winHandle);
+        }
+    }
 }
